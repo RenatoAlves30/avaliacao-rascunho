06e8719755c3e7f3c8f6c0c2b7c4f34575742609
Renato Alves <RenatoYasha@gmail.com>
2021-06-01 09:42:40 -0300

    Rascunho inicial sem os tratamentos de exceções e separação ideal de classes.

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,8 +11,6 @@ import java.util.*;
 public class Program {
     public static void main(String[] args) {
 
-
-
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
